efcd3f3a69c271879474e108ccce2424336c8859
Ali Choumane <ali.choumane@bial-x.com>
2024-07-08 08:52:56 +0200

    rename

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:i3350_project2/main.dart';
+import 'package:flutter_intro_stateful_widget/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
